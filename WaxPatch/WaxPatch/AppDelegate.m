fbddb9572092fdb9295558737ae0a6b8c6aab8af
boguang <allen.wang@b5m.com>
2015-03-11 14:30:47 +0800

    commit for backup

--- WaxPatch/WaxPatch/AppDelegate.m
+++ WaxPatch/WaxPatch/AppDelegate.m
@@ -41,41 +41,37 @@
     self.window.backgroundColor = [UIColor whiteColor];
     self.window.rootViewController = [[UIViewController alloc] init];
     [self.window makeKeyAndVisible];
-    
-    [[[UIAlertView alloc] initWithTitle:@"WaxPatch" message:@"This is the obj-c impl of a simple table view. Press [Load] button to load the wax patch and run from lua." delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Load", nil] show];
-    
+    [self dealWithPatch];
     return YES;
 }
 
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-    if(buttonIndex == [alertView firstOtherButtonIndex]) {
+- (void)dealWithPatch {
         // you probably want to change this url before run
-        NSURL *patchUrl = [NSURL URLWithString:WAX_PATCH_URL];
-        NSData *data = [NSURLConnection sendSynchronousRequest:[NSURLRequest requestWithURL:patchUrl] returningResponse:NULL error:NULL];
-        if(data) {
-            NSString *doc = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-            
-            NSString *patchZip = [doc stringByAppendingPathComponent:@"patch.zip"];
-            [data writeToFile:patchZip atomically:YES];
-            
-            NSString *dir = [doc stringByAppendingPathComponent:@"lua"];
-            [[NSFileManager defaultManager] removeItemAtPath:dir error:NULL];
-            [[NSFileManager defaultManager] createDirectoryAtPath:dir withIntermediateDirectories:YES attributes:nil error:NULL];
-            
-            ZipArchive *zip = [[ZipArchive alloc] init];
-            [zip UnzipOpenFile:patchZip];
-            [zip UnzipFileTo:dir overWrite:YES];
-            
-            NSString *pp = [[NSString alloc ] initWithFormat:@"%@/?.lua;%@/?/init.lua;", dir, dir];
-            setenv(LUA_PATH, [pp UTF8String], 1);
-            wax_start("patch", nil);
-            
-            // reinit MainViewController again
-            self.window.rootViewController = [[MainViewController alloc] init];
-            [self.window makeKeyAndVisible];
-        } else {
-            [[[UIAlertView alloc] initWithTitle:nil message:[NSString stringWithFormat:@"Fail to download wax patch from %@", patchUrl] delegate:nil cancelButtonTitle:@"Close" otherButtonTitles:nil] show];
-        }
+    NSURL *patchUrl = [NSURL URLWithString:WAX_PATCH_URL];
+    NSData *data = [NSURLConnection sendSynchronousRequest:[NSURLRequest requestWithURL:patchUrl] returningResponse:NULL error:NULL];
+    if(data) {
+        NSString *doc = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+        
+        NSString *patchZip = [doc stringByAppendingPathComponent:@"patch.zip"];
+        [data writeToFile:patchZip atomically:YES];
+        
+        NSString *dir = [doc stringByAppendingPathComponent:@"lua"];
+        [[NSFileManager defaultManager] removeItemAtPath:dir error:NULL];
+        [[NSFileManager defaultManager] createDirectoryAtPath:dir withIntermediateDirectories:YES attributes:nil error:NULL];
+        
+        ZipArchive *zip = [[ZipArchive alloc] init];
+        [zip UnzipOpenFile:patchZip];
+        [zip UnzipFileTo:dir overWrite:YES];
+        
+        NSString *pp = [[NSString alloc ] initWithFormat:@"%@/?.lua;%@/?/init.lua;", dir, dir];
+        setenv(LUA_PATH, [pp UTF8String], 1);
+        wax_start("patch", nil);
+        
+        // reinit MainViewController again
+        self.window.rootViewController = [[MainViewController alloc] init];
+        [self.window makeKeyAndVisible];
+    } else {
+        [[[UIAlertView alloc] initWithTitle:nil message:[NSString stringWithFormat:@"Fail to download wax patch from %@", patchUrl] delegate:nil cancelButtonTitle:@"Close" otherButtonTitles:nil] show];
     }
 }
 
